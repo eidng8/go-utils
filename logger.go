926c8aee64d99f80decfa6211bc06729b68913c0
eidng8 <jackey.cheung@qq.com>
2024-12-03 15:22:36 +0800

    add SimpleTaggedLog

---
+++ logger.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	lg "log"
+)
+
+type SimpleTaggedLog struct {
+	logger *lg.Logger
+	Debug  bool
+}
+
+func NewLogger() SimpleTaggedLog {
+	return WrapLogger(lg.Default(), false)
+}
+
+func NewDebugLogger() SimpleTaggedLog {
+	return WrapLogger(lg.Default(), true)
+}
+
+func WrapLogger(logger *lg.Logger, debug bool) SimpleTaggedLog {
+	return SimpleTaggedLog{logger: logger, Debug: debug}
+}
+
+func (l SimpleTaggedLog) Debugf(format string, args ...interface{}) {
+	if l.Debug {
+		l.logger.Printf("[DEBUG] "+format, args...)
+	}
+}
+
+func (l SimpleTaggedLog) Errorf(format string, args ...interface{}) {
+	l.logger.Printf("[ERROR] "+format, args...)
+}
+
+func (l SimpleTaggedLog) Infof(format string, args ...interface{}) {
+	l.logger.Printf("[INFO] "+format, args...)
+}
+
+func (l SimpleTaggedLog) Panicf(format string, args ...interface{}) {
+	l.logger.Panicf("[PANIC] "+format, args...)
+}
+
+func (l SimpleTaggedLog) PanicIfError(err error) {
+	if err != nil {
+		l.logger.Panicf("[PANIC] " + err.Error())
+	}
+}
