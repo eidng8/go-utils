399f056c58497fafd600843677fb2350f0d6ea29
eidng8 <jackey.cheung@qq.com>
2024-12-11 17:58:37 +0800

    do test

--- slices_test.go
+++ slices_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,4 +22,44 @@ func Test_Pluck(t *testing.T) {
 
 func Test_JoinInteger(t *testing.T) {
 	assert.Equal(t, "1,2,3", JoinInteger([]int{1, 2, 3}, ","))
+}
+
+func Test_SliceMapFunc(t *testing.T) {
+	sut1 := []int{1, 2, 3}
+	r, err := SliceMapFunc(
+		sut1, func(i int) (string, error) { return strconv.Itoa(i), nil },
+	)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"1", "2", "3"}, r)
+}
+
+func Test_SliceMapFunc_returns_error(t *testing.T) {
+	sut1 := []int{1, 2, 3}
+	r, err := SliceMapFunc(
+		sut1, func(i int) (string, error) { return "", errors.New("test") },
+	)
+	assert.NotNil(t, err)
+	assert.Equal(t, "test", err.Error())
+	assert.Empty(t, r)
+}
+
+func Test_MapToType(t *testing.T) {
+	var (
+		sut1 interface{} = 1
+		sut2 interface{} = 2
+		sut              = []interface{}{sut1, sut2}
+	)
+	r, err := SliceMapFunc(sut, MapToType[int])
+	assert.Nil(t, err)
+	assert.Equal(t, []int{1, 2}, r)
+}
+
+func Test_MapToType_returns_error(t *testing.T) {
+	var (
+		sut1 interface{} = "1"
+		sut2 interface{} = "2"
+		sut              = []interface{}{sut1, sut2}
+	)
+	_, err := SliceMapFunc(sut, MapToType[int])
+	assert.NotNil(t, err)
 }
