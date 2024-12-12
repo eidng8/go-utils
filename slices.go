73e0944191fb5fcb2687f416791771f911caed45
eidng8 <jackey.cheung@qq.com>
2024-12-12 09:18:59 +0800

    add doc comment

--- slices.go
+++ slices.go
@@ -12,6 +12,9 @@ type Integer interface {
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
+// Pluck extracts a list of values from a slice of structs.
+// The predicate function fn is applied to each element of the slice, and the
+// result is stored in a new slice.
 func Pluck[I interface{ ~[]V }, V any, T any](
 	a I, fn func(V) T,
 ) []T {
@@ -22,6 +25,8 @@ func Pluck[I interface{ ~[]V }, V any, T any](
 	return r
 }
 
+// JoinInteger converts a slice of integers to a string, joining them with the
+// separator.
 func JoinInteger[A interface{ ~[]T }, T Integer](a A, sep string) string {
 	e := make([]string, len(a))
 	for i, v := range a {
@@ -30,6 +35,9 @@ func JoinInteger[A interface{ ~[]T }, T Integer](a A, sep string) string {
 	return strings.Join(e, sep)
 }
 
+// SliceMapFunc applies the predicate function to each element of the slice,
+// returning a new slice with the results. If the predicate function returns an
+// error, the function stops and returns the error.
 func SliceMapFunc[I, O any](a []I, fn func(I) (O, error)) ([]O, error) {
 	var err error
 	r := make([]O, len(a))
@@ -42,6 +50,20 @@ func SliceMapFunc[I, O any](a []I, fn func(I) (O, error)) ([]O, error) {
 	return r, nil
 }
 
+// SliceFindFunc returns the first element in the slice that satisfies the
+// predicate function, or the zero value of the element type if none is found.
+func SliceFindFunc[I any](a []I, fn func(I) bool) I {
+	var zero I
+	for _, v := range a {
+		if fn(v) {
+			return v
+		}
+	}
+	return zero
+}
+
+// MapToType converts an element of interface{} to an element of specific type,
+// returning an error if the conversion is not possible.
 func MapToType[T any](val any) (T, error) {
 	var zero T
 	v, ok := val.(T)
