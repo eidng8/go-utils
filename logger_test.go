5609c8eef896bfc64a1eac23539677dec37b0298
eidng8 <jackey.cheung@qq.com>
2025-01-09 10:03:57 +0800

    fix test names

--- logger_test.go
+++ logger_test.go
@@ -16,50 +16,50 @@ func setupLoggerTest() (*SimpleTaggedLog, *bytes.Buffer) {
 	return &logger, &buf
 }
 
-func TestNewLogger(t *testing.T) {
+func Test_NewLogger(t *testing.T) {
 	logger := NewLogger()
 	require.Equal(t, log.Default(), logger.logger)
 	require.False(t, logger.Debug)
 }
 
-func TestNewDebugLogger(t *testing.T) {
+func Test_NewDebugLogger(t *testing.T) {
 	logger := NewDebugLogger()
 	require.Equal(t, log.Default(), logger.logger)
 	require.True(t, logger.Debug)
 }
 
-func TestDebugf(t *testing.T) {
+func Test_Debugf(t *testing.T) {
 	logger, buf := setupLoggerTest()
 	logger.Debugf("test %d", 1)
 	require.Equal(t, "[DEBUG] test 1\n", buf.String())
 }
 
-func TestDebugf_Disabled(t *testing.T) {
+func Test_Debugf_Disabled(t *testing.T) {
 	var buf bytes.Buffer
 	logger := WrapLogger(log.New(io.Writer(&buf), "", 0), false)
 	logger.Debugf("test %d", 1)
 	require.Empty(t, buf)
 }
 
-func TestErrorf(t *testing.T) {
+func Test_Errorf(t *testing.T) {
 	logger, buf := setupLoggerTest()
 	logger.Errorf("test %d", 1)
 	require.Equal(t, "[ERROR] test 1\n", buf.String())
 }
 
-func TestInfof(t *testing.T) {
+func Test_Infof(t *testing.T) {
 	logger, buf := setupLoggerTest()
 	logger.Infof("test %d", 1)
 	require.Equal(t, "[INFO] test 1\n", buf.String())
 }
 
-func TestPanicf(t *testing.T) {
+func Test_Panicf(t *testing.T) {
 	logger, buf := setupLoggerTest()
 	require.Panics(t, func() { logger.Panicf("test %d", 1) })
 	require.Equal(t, "[PANIC] test 1\n", buf.String())
 }
 
-func TestLoggerPanicIfError(t *testing.T) {
+func Test_LoggerPanicIfError(t *testing.T) {
 	logger, buf := setupLoggerTest()
 	require.Panics(t, func() { logger.PanicIfError(assert.AnError) })
 	require.Equal(t, "[PANIC] "+assert.AnError.Error()+"\n", buf.String())
